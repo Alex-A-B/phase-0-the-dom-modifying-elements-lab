08e21f990d8298472d70ae90786fdfc7634b4474
Alex-A-B <alexjamesbelton@gmail.com>
2021-04-05 09:50:03 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+//use querySelector to find element#ID then .remove() it
+
+document.querySelector('main#main').remove();
+
+// create a header var called newHeader that points to h1#victory
+// remember to link it to the document first! 
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = "victory"
+
+newHeader.innerHTML = "Alex is the champion"
